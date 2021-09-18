8ca04e135d185e108e6d61b424aba1325cff7ea1
PaulBautin <pbautin70@gmail.com>
2021-09-18 14:15:03 -0400

    - creation d'un venv - update pour fonctionner avec TensorFlow v2 - update explication pour faire tourner le programme

--- OCRT.py
+++ OCRT.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, division
 import numpy as np
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_eager_execution()
 import cv2
 from scipy.fftpack import fft, fftshift, ifft, ifftshift
 import scipy.io
